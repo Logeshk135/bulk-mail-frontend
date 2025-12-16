8215b66a078bd6f9d50b7922ed678dee8afb2eef
Logeshk135 <Logeshk135@gmail.com>
2025-12-16 16:38:30 +0530

    bulkmail frontend

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ function App() {
 
  const Send = () => {
     setstatus(true);
-   axios.post("https://bulk-mail-backend-64aa.onrender.com/sendemail",{msg:msg,emailList:emailList})
+   axios.post(process.env.BACKEND_URL+"/sendemail",{msg:msg,emailList:emailList})
    .then(function(data)
   {
     if(data.data === true)
