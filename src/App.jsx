8ed45ec573428fc9bc1ac861b7ad728619be5b9a
Logeshk135 <Logeshk135@gmail.com>
2025-12-22 14:42:40 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ function App() {
 
  const Send = () => {
     setstatus(true);
-   axios.post("https://bulk-mail-backend-9jsh.onrender.com/sendemail",{msg:msg,emailList:emailList})
+   axios.post(process.env.BACKEND_URL,{msg:msg,emailList:emailList})
    .then(function(data)
   {
     if(data.data === true)
