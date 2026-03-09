225bfcc332bfe7918205a3f87b49546718ed07fc
Logeshk135 <Logeshk135@gmail.com>
2026-03-09 16:20:49 +0530

    app.js

--- src/App.jsx
+++ src/App.jsx
@@ -4,57 +4,54 @@ import * as XLSX from 'xlsx';
 
 
 function App() {
- const [msg, setMsg] = useState('')
+  const [msg, setMsg] = useState('')
   const [status, setstatus] = useState(false);
   const [emailList, setEmailList] = useState([]);
 
- function handlemsg(event) 
- {
+  function handlemsg(event) {
     setMsg(event.target.value)
- }
+  }
 
- function handlefile(event)
- {
-   const file=event.target.files[0]
-   console.log(file);
+  function handlefile(event) {
+    const file = event.target.files[0]
+    console.log(file);
 
     const reader = new FileReader();
 
-    reader.onload = function(event) {
-        const data = event.target.result;
-        const workbook = XLSX.read(data, { type: 'binary' });
-        const SheetName = workbook.SheetNames[0];
-        const worksheet = workbook.Sheets[SheetName];
-        const emailList = XLSX.utils.sheet_to_json(worksheet, {header: "A" });
-        const totalemail = emailList.map(function(item) {
-          return item.A;
-        })
-        console.log(totalemail);
-        setEmailList(totalemail);
+    reader.onload = function (event) {
+      const data = event.target.result;
+      const workbook = XLSX.read(data, { type: 'binary' });
+      const SheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[SheetName];
+      const emailList = XLSX.utils.sheet_to_json(worksheet, { header: "A" });
+      const totalemail = emailList.map(function (item) {
+        return item.A;
+      })
+      console.log(totalemail);
+      setEmailList(totalemail);
     };
 
     reader.readAsArrayBuffer(file);
 
- }
+  }
 
 
- const Send = () => {
+  const Send = () => {
     setstatus(true);
-    axios.post("https://bulk-mail-backend-two.vercel.app/sendemail",{msg:msg,emailList:emailList})
-   .then(function(data)
-  {
-    if(data.data === true)
-    {
-      alert("Email sent successfully");
-      setstatus(false);
-      window.location.reload(); 
-    }
-    else
-    {
-      alert("Failed");
-    }
-   });
-      
+    axios.post("https://bulk-mail-backend-two.vercel.app/sendemail", { msg: msg, emailList: emailList })
+      .then(function (data) {
+        if (data.data === true) {
+          alert("Email sent successfully");
+          setstatus(false);
+
+          setMsg("");
+          setEmailList([]);
+        }
+        else {
+          alert("Failed");
+        }
+      });
+
   };
 
   return (
@@ -70,12 +67,12 @@ function App() {
       </div>
 
       <div className='bg-blue-600 flex flex-col items-center text-black px-5 py-3'>
-        <textarea onChange={handlemsg} value={msg}className='w-[80%] h-32 py-2 px-3  outline-none bg-white border rounded-md' placeholder='Enter the email text...'></textarea>
+        <textarea onChange={handlemsg} value={msg} className='w-[80%] h-32 py-2 px-3  outline-none bg-white border rounded-md' placeholder='Enter the email text...'></textarea>
         <div>
-          <input type="file" onChange={handlefile} className="border-4 border-dashed  py-4 mt-5 mb-5"/>
+          <input type="file" onChange={handlefile} className="border-4 border-dashed  py-4 mt-5 mb-5" />
         </div>
-         <p className='font-bold text-l'>Total Email in the Files:{emailList.length}</p>
-          <button  onClick={Send} className=' bg-blue-950 text-white font-bold px-5 py-2 rounded-md mt-5 mb-5'>{status?"sending...":"Send"}</button>
+        <p className='font-bold text-l'>Total Email in the Files:{emailList.length}</p>
+        <button onClick={Send} className=' bg-blue-950 text-white font-bold px-5 py-2 rounded-md mt-5 mb-5'>{status ? "sending..." : "Send"}</button>
       </div>
     </div>
   )
